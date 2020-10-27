9ac36e2a10793c238fd72fcbfb2d22d7e009c2a5
Diego <diego.perales@ucb.edu.bo>
2020-10-27 14:33:49 -0400

    subir ejercicio 1 de practico_funciones

---
+++ practico_funciones/ejercicio_1.py
@@ -0,0 +1,10 @@
+#
+n1 = int(input("ingrese el primer numero:"))
+n2 = int(input("ingrese el segundo numero:"))
+
+if n1 > n2:
+    print("el numero mayor es el primer numero. Numero:",n1)
+if n1 < n2:
+    print("el numero mayor es el segundo. Numero:",n2)
+else:
+    print("todo los numeros son iguales")
