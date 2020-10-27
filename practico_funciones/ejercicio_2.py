30d8ebb25efa5fd8f9007cffd6ccb0533137703c
Diego <diego.perales@ucb.edu.bo>
2020-10-27 14:42:54 -0400

    subir ejercicio 2 de practico_funciones

--- practico_funciones/ejercicio_2.py
+++ practico_funciones/ejercicio_2.py
@@ -9,5 +9,3 @@ if n1 < n2 and n2 > n3:
     print("el numero mayor es el segundo. Numero:",n2)
 if n1 < n3 and n3 > n2:
     print("el numero mayor es el tercer numero. Numero",n3)
-else:
-    print("todo los numeros son iguales")
