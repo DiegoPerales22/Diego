bb8aa6dd47d798fa720bd9692f9764694aa4c576
Diego <diego.perales@ucb.edu.bo>
2020-11-24 23:29:31 -0400

    subir ejercicio 2 de vectores

---
+++ vectores/ejercicio_2.py
@@ -0,0 +1,14 @@
+#ejercicio 2
+vector1=[1, 2, 3, 4, 5]
+vector2=[5, 6, 7, 8, 9]
+
+
+vectorSuma=[]
+for i in range(0,5):
+    num=vector1[i]+vector2[i]
+    vectorSuma.append(num)
+
+
+print(vector1)
+print(vector2)
+print(vectorSuma)
